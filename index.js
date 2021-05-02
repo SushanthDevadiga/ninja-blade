aa46fdcba16d3fea6f9a882fe656aaa0cb4d5e57
SushanthDevadiga <83549041+SushanthDevadiga@users.noreply.github.com>
2021-05-02 22:24:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<h1>Combat Hero</h1>
+<canvas id="my-canvas" width="800" height="500"></canvas>
+<abbr title="backward(down arrow)"><button id="backward"><img width="100" height="100" src="https://cdn.glitch.com/4f8fd709-0c34-49fd-bf23-8322628599fb%2Fbackward?v=1619883429765"></button></abbr>
+<abbr title="kick(left arrow)"><button id="kick"><img width="100" src="https://cdn.glitch.com/4f8fd709-0c34-49fd-bf23-8322628599fb%2Fkick.symbol.jpg?v=1619883413567"></button></abbr>
+<abbr title="block(space bar)"><button id="block"><img width="100" src="https://cdn.glitch.com/4f8fd709-0c34-49fd-bf23-8322628599fb%2Fblock.jpg?v=1619883874630"></button></abbr>
+<abbr title="punch(right arrow)"><button id="punch"><img width="100" src="https://cdn.glitch.com/4f8fd709-0c34-49fd-bf23-8322628599fb%2Fpunch.symbol.jpg?v=1619883421088"></button></abbr>
+<abbr title="forward(up arrow)"><button id="forward"><img width="100" height="100" src="https://cdn.glitch.com/4f8fd709-0c34-49fd-bf23-8322628599fb%2Fforward?v=1619883438982"></button></abbr>
+<style>
+  
+</style>
+<h1 align="right">
+   Use these buttons/arrows to play & have fun!
+</h1>
+
+<script type="text/javascript" src="video.js"></script>
